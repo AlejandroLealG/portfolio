d557830be40f8ba5ddcb3e70cd1b47fb5e2b78a3
AlejandroLealGonzalez <alejandro.leal05@outlook.com>
2024-09-09 20:56:39 -0500

    Creacion del header, pendiente logo

---
+++ src/app/components/header/Header.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function Header() {
+
+  const [bgColor, setBgColor] = useState('bg-transparent');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100){
+        setBgColor('bg-[#1f2937e6]');
+      } else {
+        setBgColor('bg-transparent');
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
+  return (
+    <div className="sticky top-4 text-white my-4">
+    <nav className={`flex p-4 justify-between mx-36 duration-500 rounded-full ${bgColor}`}>
+      {/* <img className="w-28 h-12" src="/logo.png"/> */}
+      <h1 className="text-center">ALG</h1>
+      <ul className="flex flex-row gap-4 px-8 hover:cursor-default text-center items-center ">
+        <li className="hover:text-[#f3c242]">Cualidades</li>
+        <li className="hover:text-[#f3c242]">Proyectos</li>
+        <li className="hover:text-[#f3c242]">Sobre mi</li>
+        <li className="hover:text-[#f3c242]">Contacto</li>
+      </ul>
+    </nav>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import Header from '@/app/components/header/Header'
+
 export default function Home() {
   return (
-    <main>
-      <p>header</p>
+    <main className='h-[3500px]'>
+      <Header />
       <p>yo (contactame)</p>
       <p>cualidades</p>
       <p>proyectos</p>
       <p>sobre mi</p>
+      <p>contacto</p>
       <p>footer</p>
     </main>
   );
